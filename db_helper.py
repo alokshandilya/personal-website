6c2907a25d42b76265da6f4ce031471c19c804fe
Alok Shandilya <alok.shandilya10@gmail.com>
2024-05-19 12:07:39 +0530

    create .env

--- db_helper.py
+++ db_helper.py
@@ -1,6 +1,11 @@
-from pymongo.mongo_client import MongoClient
+# import os
+# from dotenv import load_dotenv
 from pymongo.server_api import ServerApi
+from pymongo.mongo_client import MongoClient
 
+# db_password = os.getenv("DB_PASSWORD")
+
+# uri = "mongodb+srv://alokshandilya:{db_password}@cluster0.snyhuvn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 uri = "mongodb+srv://alokshandilya:V9Ow23uoUwD41rca@cluster0.snyhuvn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 # Create a new client and connect to the server
